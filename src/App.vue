6170e4fc72bfef8eb7d448ea3057b4d1da5fef29
Soulaimane <boukrabsolayman402@gmail.com>
2024-03-28 11:21:19 +0000

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRecipeStore } from '@/stores/recipes';
+import Navbar from '@/components/Navbar.vue';
+
+const recipeStore = useRecipeStore();
+
+// Fetch recipes from the API after the component is mounted
+onMounted(() => {
+  recipeStore.fetchRecipes();
+});
+</script>
+
+<template>
+  <Navbar />
+
+  <RouterView />
+</template>
